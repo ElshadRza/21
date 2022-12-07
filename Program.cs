1e5284718b7d93adea8c1d9eb371394eb80773cd
berserk222 <118837671+berserk222@users.noreply.github.com>
2022-12-07 17:48:08 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Eded daxil edin");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("Duzgun eded daxil edin");
+}
+
+else if (n % 21 == 0) {
+    Console.WriteLine("eded 3-e ve 7-e bolunur");
+
+}
+else if (n % 21 != 0) {
+    Console.WriteLine("eded 3-e ve 7-e bolunmur");
+}
+
+
+
