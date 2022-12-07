b8b38641bb43e625da3ea8c7eda6ea5950ccceb6
berserk222 <118837671+berserk222@users.noreply.github.com>
2022-12-07 17:52:45 +0400

    Add files via upload

---
+++ Studentgrade.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Reqem daxil edin");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+
+if (grade < 0 && grade > 100)
+{
+    Console.WriteLine("Duzgun reqem daxil edin");
+}
+
+else if (grade > 90 && grade < 101)
+{
+    Console.WriteLine("A");
+}
+
+else if (grade > 80 && grade < 91)
+{
+    Console.WriteLine("B");
+}
+
+else if (grade > 70 && grade < 81)
+{
+    Console.WriteLine("C");
+}
+else if (grade > 60 && grade < 71)
+{
+    Console.WriteLine("D");
+}
+else if (grade > 50 && grade < 61)
+{
+    Console.WriteLine("E");
+}
+
+else  
+{
+    Console.WriteLine("Kesildiniz");
+}
